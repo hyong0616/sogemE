b173ba7216cd49688f69d484214621e077869c72
hyo <hyong0616@naver.com>
2020-09-15 22:03:06 +0900

    init project

---
+++ src/main/java/com/hanium/sogemE/sogemE/SogemEApplication.java
@@ -0,0 +1,13 @@
+package com.hanium.sogemE.sogemE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SogemEApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SogemEApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanium/sogemE/sogemE/SogemEApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanium.sogemE.sogemE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogemEApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
